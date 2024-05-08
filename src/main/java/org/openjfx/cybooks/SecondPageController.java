b1cf848744b5d55fe8d9cc61f9eec6cfd673f5fe
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2024-05-08 12:13:45 +0200

    Update SecondPageController.java

--- src/main/java/org/openjfx/cybooks/SecondPageController.java
+++ src/main/java/org/openjfx/cybooks/SecondPageController.java
@@ -9,19 +9,5 @@ import java.util.Objects;
 
 public class SecondPageController {
 
-    @FXML
-    private Label headerLabel;
-    @FXML
-    private ImageView book;
 
-    @FXML
-    protected void initialize() {
-        // Load and set the image
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/openjfx/cybooks/img/enchanted_book.gif")));
-        book.setImage(image);
-        // Set the fit width and height
-        book.setFitWidth(25);
-        book.setFitHeight(25);
-
-    }
 }
