bff1e75b3ffa36ebaecb8e8362a10ae330167d79
Jagadai <106830744+Jagadai@users.noreply.github.com>
2024-05-13 19:47:58 +0200

    Ajout des variables
    a faire les fonctions (en cours)

---
+++ src/Librarian.java
@@ -0,0 +1,18 @@
+public class Librarian {
+
+    protected int id;
+    protected String firstName;
+    protected String lastName;
+
+    protected int getid(){
+        return id;
+    }
+
+    protected String getfirstName(){
+        return firstName;
+    }
+
+    protected String getlastName(){
+        return lastName;
+    }
+}
--- src/Loan.java
+++ src/Loan.java
@@ -1,2 +1,38 @@
-public class Loan {
+public class Loan extends Core {
+
+    protected int id;
+    protected int ISBN;
+    protected int userid;
+    protected int beginDate;        //written in millisecond
+    protected int duration;
+    protected boolean expired;
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getBeginDate() {
+        return beginDate;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public boolean getExpired() {
+        return expired;
+    }
 }
+
