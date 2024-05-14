5cdb8fe199eee9a0d973bac1ea019977708899a9
theo-be <belliere.theo@outlook.fr>
2024-05-14 16:49:11 +0200

    Added module JDBC

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module org.openjfx.cybooks {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.sql;
+    requires mysql.connector.java;
 
     opens org.openjfx.cybooks to javafx.fxml;
     exports org.openjfx.cybooks;
