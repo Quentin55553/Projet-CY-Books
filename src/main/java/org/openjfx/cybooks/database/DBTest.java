9feaff72d411bbc4736763294c7c02c7333ab6b7
theo-be <belliere.theo@outlook.fr>
2024-05-16 12:41:56 +0200

    SQL queries now work

--- src/main/java/org/openjfx/cybooks/database/DBTest.java
+++ src/main/java/org/openjfx/cybooks/database/DBTest.java
@@ -1,26 +1,46 @@
 package org.openjfx.cybooks.database;
 
 
+import org.openjfx.cybooks.data.Book;
+import org.openjfx.cybooks.data.Customer;
+import org.openjfx.cybooks.data.Loan;
+
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class DBTest {
 
     public static void main(String[] args) {
 
+        List<Book> books;
+        List<Loan> loans;
+        List<Customer> customers;
         try {
-//            DBHandler.getBook("22784");
+//            System.out.println(DBHandler.getBook("22784"));
 //            List<Loan> loans = DBHandler.getLoans();
-//            for (Loan l : loans) {
-//                System.out.println(l);
+
+
+
+//            customers = DBHandler.getCustomers("Be");
+//            for (Customer c : customers) {
+//                System.out.println(c);
 //            }
 
+            DBHandler.updateLoan(5, true);
+
+            loans = DBHandler.getOngoingLoans();
 
 
-//            DBHandler.addLoan(1, 2, 5);
+//            DBHandler.addCustomer("Bel", "Theo");
+//            books = DBHandler.getMostPopularBooks();
+//            books = DBHandler.getMostPopularBooksSince("2024-05-14");
 
-//            DBHandler.addCustomer("Be", "Theo");
-
-            DBHandler.getMostPopularBooks();
+//            for (Book b : books) {
+//                System.out.println(b);
+//            }
+            for (Loan l : loans) {
+                System.out.println(l);
+            }
 
         } catch (NoSuchElementException e) {
             System.out.println(e);
