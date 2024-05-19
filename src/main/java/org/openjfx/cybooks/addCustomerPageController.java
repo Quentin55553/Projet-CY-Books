19df8006af288063934bf8a2ed4c9acf309e9ae7
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-19 23:30:38 +0200

    Ajout du controller pour la page d'inscription membre

---
+++ src/main/java/org/openjfx/cybooks/addCustomerPageController.java
@@ -0,0 +1,54 @@
+package org.openjfx.cybooks;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import org.controlsfx.control.textfield.CustomTextField;
+
+
+public class addCustomerPageController {
+    private Main main;
+    private Stage primaryStage;
+
+    @FXML
+    public CustomTextField firstname;
+    @FXML
+    public CustomTextField lastname;
+    @FXML
+    public CustomTextField telephone;
+    @FXML
+    public CustomTextField email;
+    @FXML
+    public CustomTextField address;
+
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+
+    // Setter for the primary stage
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+
+    public boolean isValidPhoneNumber(String tel) {
+
+    }
+
+
+    public boolean isValidEmail(String email) {
+
+    }
+
+
+    public boolean isValidCustomer(String address) {
+
+    }
+
+
+    @FXML
+    public void addCustomerClicked() {
+
+    }
+}
