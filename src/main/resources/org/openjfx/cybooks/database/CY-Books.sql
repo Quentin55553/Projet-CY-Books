3c531c8d9cc04b2da6a576d6d20423cea6fb0bd6
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-19 20:04:30 +0200

    Fichier rendu plus concis

--- src/main/resources/org/openjfx/cybooks/database/CY-Books.sql
+++ src/main/resources/org/openjfx/cybooks/database/CY-Books.sql
@@ -1,184 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: May 19, 2024 at 06:59 PM
--- Server version: 10.6.16-MariaDB-0ubuntu0.22.04.1
--- PHP Version: 8.1.2-1ubuntu2.17
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `CY-Books`
 --
-CREATE DATABASE IF NOT EXISTS `CY-Books` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+DROP DATABASE IF EXISTS `CY-Books`;
+
+CREATE DATABASE `CY-Books`;
 USE `CY-Books`;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `books`
---
+-- -----------------------------------------------------------
+
 
 CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `quantity` int(11) DEFAULT 0,
   `stock` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
---
--- Dumping data for table `books`
---
 
 INSERT INTO `books` (`id`, `quantity`, `stock`) VALUES
 (22784, 2, 0),
 (28737, 5, 2);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `customers`
---
+-- -----------------------------------------------------------
+
 
 CREATE TABLE `customers` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `last_name` varchar(50) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `tel` varchar(12) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `address` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
---
--- Dumping data for table `customers`
---
 
-INSERT INTO `customers` (`id`, `last_name`, `first_name`, `tel`, `email`, `address`) VALUES
-(1, 'yo', 'yo', NULL, NULL, NULL),
-(2, 'oui', 'non', NULL, NULL, NULL),
-(3, 'Bel', 'Theo', NULL, NULL, NULL);
+INSERT INTO `customers` (`last_name`, `first_name`, `tel`, `email`, `address`) VALUES
+('FILLION', 'Quentin', '0601020304', 'example@gmail.com', 'adresse1'),
+('oui', 'non', NULL, NULL, NULL),
+('Bel', 'Theo', '0601020304', 'example@gmail.com', 'adresse2');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `librarians`
---
+-- -----------------------------------------------------------
+
 
 CREATE TABLE `librarians` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `last_name` varchar(50) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `login` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `password` varchar(100) NOT NULL,
+  UNIQUE KEY `login` (`login`)
+);
 
---
--- Dumping data for table `librarians`
---
 
-INSERT INTO `librarians` (`id`, `last_name`, `first_name`, `login`, `password`) VALUES
-(2, 'admin', 'admin', 'admin', '$2a$10$3UUh4ZNoASpFshfQFMMMx.Rb02A0CfIBd70nh0XWuc4VIoTcLKqym');
+INSERT INTO `librarians` (`last_name`, `first_name`, `login`, `password`) VALUES
+('admin', 'admin', 'admin', '$2a$10$3UUh4ZNoASpFshfQFMMMx.Rb02A0CfIBd70nh0XWuc4VIoTcLKqym');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `loans`
---
+-- -----------------------------------------------------------
+
 
 CREATE TABLE `loans` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `book_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `begin_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `expiration_date` date NOT NULL,
-  `completed` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `completed` tinyint(1) DEFAULT 0,
+  KEY `book_id` (`book_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `loans_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `loans_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+);
 
---
--- Dumping data for table `loans`
---
 
-INSERT INTO `loans` (`id`, `book_id`, `customer_id`, `begin_date`, `expiration_date`, `completed`) VALUES
-(1, 22784, 1, '2024-05-14 22:00:00', '2024-05-18', 0),
-(2, 22784, 2, '2024-05-14 22:00:00', '2024-05-20', 0),
-(3, 28737, 2, '2024-05-12 22:00:00', '2024-05-16', 0),
-(4, 28737, 2, '2024-05-12 22:00:00', '2024-05-19', 1),
-(5, 28737, 2, '2024-05-12 22:00:00', '2024-05-16', 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `librarians`
---
-ALTER TABLE `librarians`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `login` (`login`);
-
---
--- Indexes for table `loans`
---
-ALTER TABLE `loans`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `book_id` (`book_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `librarians`
---
-ALTER TABLE `librarians`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `loans`
---
-ALTER TABLE `loans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `loans`
---
-ALTER TABLE `loans`
-  ADD CONSTRAINT `loans_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
-  ADD CONSTRAINT `loans_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `loans` (`book_id`, `customer_id`, `begin_date`, `expiration_date`, `completed`) VALUES
+(22784, 1, '2024-05-14', '2024-05-18', 0),
+(22784, 2, '2024-05-14', '2024-05-20', 0),
+(28737, 2, '2024-05-12', '2024-05-16', 0),
+(28737, 2, '2024-05-12', '2024-05-19', 1),
+(28737, 2, '2024-05-12', '2024-05-16', 1);
