522b41d506874cb7f14800d1702de8e152228ef1
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-21 21:19:58 +0200

    Meilleure backup jusque-là

--- src/main/java/org/openjfx/cybooks/database/IncorrectLastnameException.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.cybooks.database;
-
-
-public class IncorrectLastnameException extends IncorrectFieldException {
-    public IncorrectLastnameException(String message) {
-        super(message);
-    }
-}
