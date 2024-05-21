ab9bbf7d7939787b1d9a78e5c7094a4a618367fe
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-21 17:51:48 +0200

    Delete src/main directory

--- src/main/java/org/openjfx/cybooks/database/IncorrectEmailException.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.cybooks.database;
-
-
-public class IncorrectEmailException extends IncorrectFieldException {
-    public IncorrectEmailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/openjfx/cybooks/database/IncorrectPhoneNumberException.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.cybooks.database;
-
-
-public class IncorrectPhoneNumberException extends IncorrectFieldException {
-    public IncorrectPhoneNumberException(String message) {
-        super(message);
-    }
-}
