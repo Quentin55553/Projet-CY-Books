58470a1093b8ded8f2e4a5e176ecaba3c8f21952
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-21 16:21:46 +0200

    Ajout nbLoans

--- src/main/java/org/openjfx/cybooks/data/Customer.java
+++ src/main/java/org/openjfx/cybooks/data/Customer.java
@@ -1,5 +1,9 @@
 package org.openjfx.cybooks.data;
 
+import org.openjfx.cybooks.database.DBHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -7,75 +11,83 @@ public class Customer {
     private final int id;
     private String firstName;
     private String lastName;
-
     private String tel;
-
     private String email;
-
     private String address;
+    private int nbLoans;
 
 
-    public Customer(int id, String firstName, String lastName, String tel, String email, String address) {
+    public Customer(int id, String firstName, String lastName, String tel, String email, String address, int nbLoans) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.tel = tel;
         this.email = email;
         this.address = address;
+        this.nbLoans = nbLoans;
     }
+
 
     public int getId() {
         return id;
     }
 
-
     public String getFirstName() {
         return firstName;
     }
 
-
     public String getLastName() {
         return lastName;
-    }
-
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getTel() {
         return tel;
     }
 
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getAddress() {
         return address;
     }
 
+
+    public int getNbLoans() {
+        return nbLoans;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void setAddress(String address) {
         this.address = address;
+    }
+
+
+    public void setNbLoans(int nbLoans) {
+        this.nbLoans = nbLoans;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
+
         if (o == null || getClass() != o.getClass()) return false;
+
         Customer customer = (Customer) o;
         return id == customer.id;
     }
@@ -93,7 +105,8 @@ public class Customer {
             + "\nfirst name: " + firstName
             + "\nlast name: " + lastName
             + "\ntel: " + tel
-                + "\nemail: " + email
-                + "\naddress: " + address;
+            + "\nemail: " + email
+            + "\naddress: " + address
+            + "\nnumber of loans: " + nbLoans;
     }
 }
