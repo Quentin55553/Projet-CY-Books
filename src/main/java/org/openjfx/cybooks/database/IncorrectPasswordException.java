ea0e3f3284bbe1f7ee2e32997cbf8496ec1ecd35
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-22 18:08:45 +0200

    Delete src/main directory

--- src/main/java/org/openjfx/cybooks/database/IncorrectPasswordException.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.cybooks.database;
-
-
-public class IncorrectPasswordException extends Exception {
-    public IncorrectPasswordException(String message) {
-        super(message);
-    }
-}
