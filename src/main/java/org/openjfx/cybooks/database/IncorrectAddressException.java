4dc8d0fe926b050938b718d65abd79f0e8e6be69
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2024-05-23 18:54:15 +0200

    Ajout Package UserInput

--- src/main/java/org/openjfx/cybooks/database/IncorrectAddressException.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.cybooks.database;
-
-
-public class IncorrectAddressException extends IncorrectFieldException {
-    public IncorrectAddressException(String message) {
-        super(message);
-    }
-}
