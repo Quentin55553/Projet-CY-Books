06bea84ac82eed8da06f024fb03250102754c835
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-05-23 23:32:31 +0200

    Héritage de IncorrectFieldException

--- src/main/java/org/openjfx/cybooks/UserInput/IncorrectPasswordException.java
+++ src/main/java/org/openjfx/cybooks/UserInput/IncorrectPasswordException.java
@@ -4,7 +4,7 @@ package org.openjfx.cybooks.UserInput;
 /**
  * This exception arises when the user enters an incorrect password
  */
-public class IncorrectPasswordException extends Exception {
+public class IncorrectPasswordException extends IncorrectFieldException {
 
     /**
      * Constructor for the IncorrectPasswordException exception
