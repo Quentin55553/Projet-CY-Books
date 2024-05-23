d46693efd2d8912bc10e05f035da1ade3c99a2f6
theo-be <belliere.theo@outlook.fr>
2024-05-23 22:13:38 +0200

    added real books in database

--- src/main/resources/org/openjfx/cybooks/database/CY-Books.sql
+++ src/main/resources/org/openjfx/cybooks/database/CY-Books.sql
@@ -18,8 +18,37 @@ CREATE TABLE `books` (
 
 
 INSERT INTO `books` (`id`, `quantity`, `stock`) VALUES
-('22784', 2, 0),
-('28737', 5, 4);
+                                                  ('12148/bc6p06wqkzq', 5, 5),
+                                                  ('12148/bc6p06zd9f2', 5, 5),
+                                                  ('12148/bc6p06zdb17', 5, 5),
+                                                  ('12148/bc6p06zdk33', 5, 5),
+                                                  ('12148/bc6p06zgx6s', 5, 5),
+                                                  ('12148/bc6p070wjcv', 5, 5),
+                                                  ('12148/bc6p070wtkd', 5, 5),
+                                                  ('12148/bc6p070z05c', 5, 5),
+                                                  ('12148/bc6p070zkng', 5, 5),
+                                                  ('12148/bc6p070zmhd', 5, 5),
+                                                  ('12148/bc6p070zp78', 5, 5),
+                                                  ('12148/bc6p0713fvn', 5, 5),
+                                                  ('12148/bc6p0714050', 5, 5),
+                                                  ('12148/bc6p07142nf', 5, 5),
+                                                  ('12148/bc6p07144dn', 5, 5),
+                                                  ('12148/bc6p07149zq', 5, 5),
+                                                  ('12148/bc6p0715d50', 5, 5),
+                                                  ('12148/bc6p0715f28', 5, 5),
+                                                  ('12148/bc6p0715jj1', 5, 5),
+                                                  ('12148/bc6p0715s5d', 5, 5),
+                                                  ('12148/bc6p0715vrw', 5, 5),
+                                                  ('12148/bc6p0715zdc', 5, 5),
+                                                  ('12148/bc6p071620f', 5, 5),
+                                                  ('12148/bc6p07163km', 5, 5),
+                                                  ('12148/bc6p0716c4m', 5, 5),
+                                                  ('12148/bpt6k33646735', 2, 0),
+                                                  ('12148/cb32729500t', 5, 5),
+                                                  ('12148/cb34348467k', 5, 4),
+                                                  ('12148/cb34372185v', 5, 5),
+                                                  ('12148/cb344012968', 5, 5),
+                                                  ('12148/cb344198366', 5, 5);
 
 
 -- -----------------------------------------------------------
@@ -31,13 +60,14 @@ CREATE TABLE `customers` (
   `first_name` varchar(50) NOT NULL,
   `tel` varchar(12) UNIQUE DEFAULT NULL,
   `email` varchar(50) UNIQUE DEFAULT NULL,
-  `address` text DEFAULT NULL
+  `address` text DEFAULT NULL,
+  `loan_count` int DEFAULT 0
 );
 
 
-INSERT INTO `customers` (`last_name`, `first_name`, `tel`, `email`, `address`) VALUES
-('FILLION', 'Quentin', '0601020304', 'quentin.fillion@gmail.com', 'adresse1'),
-('Belliere', 'Theo', '0602030405', 'theo.belliere@gmail.com', 'adresse2');
+INSERT INTO `customers` (`last_name`, `first_name`, `tel`, `email`, `address`, `loan_count`) VALUES
+('FILLION', 'Quentin', '0601020304', 'quentin.fillion@gmail.com', 'adresse1', 1),
+('Belliere', 'Theo', '0602030405', 'theo.belliere@gmail.com', 'adresse2', 4);
 
 
 -- -----------------------------------------------------------
@@ -74,8 +104,8 @@ CREATE TABLE `loans` (
 
 
 INSERT INTO `loans` (`book_id`, `customer_id`, `begin_date`, `expiration_date`, `completed`) VALUES
-(22784, 1, '2024-05-14', '2024-05-18', 0),
-(22784, 2, '2024-05-14', '2024-05-20', 0),
-(28737, 2, '2024-05-12', '2024-05-16', 0),
-(28737, 2, '2024-05-12', '2024-05-19', 1),
-(28737, 2, '2024-05-12', '2024-05-16', 1);
+('12148/bpt6k33646735', 1, '2024-05-14', '2024-05-18', 0),
+('12148/bpt6k33646735', 2, '2024-05-14', '2024-05-20', 0),
+('12148/cb34348467k', 2, '2024-05-12', '2024-05-16', 0),
+('12148/cb34348467k', 2, '2024-05-12', '2024-05-19', 1),
+('12148/cb34348467k', 2, '2024-05-12', '2024-05-16', 1);
