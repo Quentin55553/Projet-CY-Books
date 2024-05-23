32c861500a5390d016a6acead2d68fdeedf94aea
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2024-05-23 18:54:43 +0200

    Ajout Package UserInput

--- src/main/java/org/openjfx/cybooks/database/IncorrectFieldException.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.cybooks.database;
-
-
-public class IncorrectFieldException extends Exception {
-    public IncorrectFieldException(String message) {
-        super(message);
-    }
-}
