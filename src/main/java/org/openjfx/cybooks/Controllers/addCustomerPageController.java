1654c2a89b97b42a894aa6b32b68443e8249b703
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2024-05-24 18:24:51 +0200

    Commentaires

--- src/main/java/org/openjfx/cybooks/Controllers/addCustomerPageController.java
+++ src/main/java/org/openjfx/cybooks/Controllers/addCustomerPageController.java
@@ -13,34 +13,78 @@ import org.openjfx.cybooks.database.*;
 import java.sql.SQLException;
 
 
+/**
+ * Controller class for the page that allows the user to add a new customer to the database
+ */
 public class addCustomerPageController {
+
+    /**
+     * Reference to the main application
+     */
     private Main main;
+    /**
+     * Main container
+     */
     private Stage primaryStage;
 
+    /**
+     * Firstname field for the addition of a customer
+     */
     @FXML
     public CustomTextField firstnameField;
+    /**
+     * Lastname field for the addition of a customer
+     */
     @FXML
     public CustomTextField lastnameField;
+
+    /**
+     * Phone number field for the addition of a customer
+     */
     @FXML
     public CustomTextField telField;
+    /**
+     * Email field for the addition of a customer
+     */
     @FXML
     public CustomTextField emailField;
+    /**
+     * Adress field for the addition of a customer
+     */
     @FXML
     public CustomTextField addressField;
+    /**
+     * Error label for the addition of a customer
+     */
     @FXML
     public Label errorLabel;
+
+    /**
+     * Confirmation label for the addition of a customer
+     */
     @FXML
     public Label confirmationLabel;
 
+    /**
+     * Setter for the main attribute
+     * @param main Reference to the main application
+     */
     public void setMain(Main main) {
         this.main = main;
     }
 
-    // Setter for the primary stage
+    /**
+     * Setter for the primary stage attribute
+     * @param primaryStage The main container
+     */
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
+    /**
+     * This method is called when the 'Add customer' button is clicked.
+     * It checks if the entered values are correct and if they are, adds the new customer to the database
+     */
     @FXML
     public void addCustomerClicked() {
         String enteredFirstname = firstnameField.getText();
