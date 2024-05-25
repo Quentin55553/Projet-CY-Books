52dc0e8f932a2c3265bc02385b4c430c5df21a5d
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2024-05-25 13:49:32 +0200

    Commentaires

--- src/main/java/org/openjfx/cybooks/Controllers/BookPageController.java
+++ src/main/java/org/openjfx/cybooks/Controllers/BookPageController.java
@@ -6,16 +6,34 @@ import javafx.scene.control.TextField;
 import org.openjfx.cybooks.data.Book;
 import org.openjfx.cybooks.database.DBHandler;
 
+/**
+ * Controller for the page showing the information of a specific book
+ */
 public class BookPageController {
+    /**
+     * Text area used to show the authors of the book
+     */
     @FXML
     private JFXTextArea author;
+    /**
+     * Text area used to show the description of the book
+     */
     @FXML
     private JFXTextArea description;
+    /**
+     * Text area used to show the stock of the book
+     */
     @FXML
     private TextField stock;
+    /**
+     * Text area used to show the quantity of the book
+     */
     @FXML
     private TextField quantity;
 
+    /**
+     * Text area used to show the title of the book
+     */
     @FXML
     private JFXTextArea title;
     /**
@@ -23,11 +41,18 @@ public class BookPageController {
      */
     private Book book;
 
+    /**
+     * Method used to load the book's information on the page
+     * @param book
+     */
     public void setButtonBook(Book book) {
         this.book = book;
         loadBookInformation();
     }
 
+    /**
+     * This method sets the text on the fields to show the information of the book
+     */
     private void loadBookInformation() {
         title.setText(book.getTitle());
         author.setText(String.valueOf(book.getAuthors()));
@@ -36,6 +61,10 @@ public class BookPageController {
         quantity.setText(String.valueOf(book.getTotal()));
     }
 
+    /**
+     * This method handles editing of the stock
+     * The user must enter a new value and press the enter key
+     */
     @FXML
     private void editStock(){
         // Make the stock text field editable and request focus
@@ -71,6 +100,11 @@ public class BookPageController {
         });
     }
 
+    /**
+     * This method checks if the stock value entered is correct
+     * @param newStock The new stock value entered by the user
+     * @return true if the value is correct, false if not
+     */
     private boolean checkStockupdate(String newStock){
         if(newStock == null || newStock.equals("")){
             return false;
